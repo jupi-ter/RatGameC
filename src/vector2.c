292be6715fb30a3ac13fab0acc4d555fcf545533
jupi-ter <jupi-ter@users.noreply.github.com>
2026-02-03 17:44:00 -0300

    feat: vec2 distance

--- src/vector2.c
+++ src/vector2.c
@@ -1,4 +1,5 @@
 #include "vector2.h"
+#include <math.h>
 
 vec2_t vec2_add(vec2_t v1, vec2_t v2) {
     vec2_t r = { .x = v1.x + v2.x, .y = v1.y + v2.y};
@@ -8,4 +9,11 @@ vec2_t vec2_add(vec2_t v1, vec2_t v2) {
 vec2_t vec2_sub(vec2_t v1, vec2_t v2) {
     vec2_t r = { .x = v1.x - v2.x, .y = v1.y - v2.y};
     return r;
+}
+
+float vec2_distance(vec2_t v1, vec2_t v2) {
+    float s1 = v2.x - v1.x;
+    float s2 = v2.y - v1.y;
+
+    return sqrtf((s1 * s1) + (s2 * s2));
 }
